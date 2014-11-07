5f47066732a1906fbea6b1ffa34316d4fb32f474
Siarhei Barylau <sbarylau@expedia.com>
2014-11-06 16:35:49 -0800

    Adds ability to add multiple features

--- init.rb
+++ init.rb
@@ -2,119 +2,177 @@ require 'pry'
 require 'wally'
 require_relative 'parser.rb'
 require_relative 'testrail.rb'
+require 'nokogiri'
 
 ##################################################################################################################
 #   Define Variables
 
-client = TestRail::APIClient.new('https://expediabookings.testrail.com')
-client.user = 'sbarylau@expedia.com'
+
+# Expedia official project
+client = TestRail::APIClient.new('https://testrail.sea.corp.expecn.com')
+client.user = 'sbarylau'
 client.password = 'Valerik26'
+project_id = 25 # in official TestRailk
+automated_section_id = 1943 # in official TestRail
+suite_id = 1076 # in official TestRail
 
-project_id = "1"
-parent_id = "10" # Automated section in TestRail
 
-cases_ids =  [] # stores IDs of added test cases
-test_results = [] # stores test results after run
+# Exepdia Test Project
+#client = TestRail::APIClient.new('https://expediabookings.testrail.com')
+#client.user = 'sbarylau@expedia.com'
+#client.password = 'Valerik26'
+#project_id = "1" # "25" in official TestRailk
+#automated_section_id = 10  # 1943 in official TestRail
+#suite_id = 1 # 1076 in official TestRail
+
+
+
 ##################################################################################################################
-# feature = { :name => $features.first[:gherkin]["name"], :description => $features.first[:gherkin]["description"],
-#   :tags => $features.first[:gherkin]["tags"], :scenarios =>  $features.first[:gherkin]["elements"] }
+# delete all sections inside Automated section
 
-  steps = ""
-  scenarios =  $features.first[:gherkin]["elements"]
-  feature_name = $features.first[:gherkin]["name"]
-  feature_description = $features.first[:gherkin]["description"]
-  feature_tags = $features.first[:gherkin]["tags"]
-  scenarios_array =  $features.first[:gherkin]["elements"]
-  first_scenario = $features.first[:gherkin]["elements"][0]
-  first_scenario_name = $features.first[:gherkin]["elements"][0]["name"]
-  first_scenario_tags = $features.first[:gherkin]["elements"][0]["tags"]
-  first_scenario_type = $features.first[:gherkin]["elements"][0]["type"]
-  first_scenario_steps_array = $features.first[:gherkin]["elements"][0]["steps"]
-  first_scenario_steps_array.each do |a|
-    steps << "#{a["keyword"]}#{a["name"]}\n"
-  end
+all_sections = client.send_get("get_sections/#{project_id}&suite_id=#{suite_id}")
+to_delete_ids = all_sections.select { |name| name["parent_id"] == automated_section_id }
 
-  section_params =  {
-    "name" => feature_name,
-    "description" => feature_description,
-    "parent_id" => parent_id
-  }
-  
+puts ""
+
+delete_section = []
+to_delete_ids.each do |delete_id|
+  delete_section << client.send_post("delete_section/#{delete_id['id']}", {"suite_id" => "#{suite_id}"})
+end
+
+puts "Automated section cleaned!!!"
 ##################################################################################################################
-#   Create Section  
-  
-    post_section = client.send_post("add_section/#{project_id}", section_params) 
+##################################################################################################################
+
+
+$features.each do |feature|  # global features DO
+
+    $cases_ids =  [] # stores IDs of added test cases
+    test_results = [] # stores test results after run
+    steps = ""
+    scenarios =  feature[:gherkin]["elements"]
+    feature_name = feature[:gherkin]["name"]
+    feature_description = feature[:gherkin]["description"]
+    feature_tags = feature[:gherkin]["tags"]
+    scenarios_array =  feature[:gherkin]["elements"]
+
+
+    section_params =  {
+        "name" => feature_name,
+        "description" => feature_description,
+        "parent_id" => automated_section_id,
+        "suite_id" => suite_id
+    }
+
+    puts "Variables initialized"
+
+    ##################################################################################################################
+    #   Create Section
+
+    post_section = client.send_post("add_section/#{project_id}", section_params)
+    puts "Section added: #{post_section}"
     section_id =  post_section["id"]
 
-##################################################################################################################
-# Add Test Cases to earlier created section
+    puts "Test suite created in TestRail"
+    ##################################################################################################################
+    # Add Test Cases to earlier created section
 
     scenarios_array.each do |scenario|
-     scenario_name = scenario["name"]
-     scenario_steps_array = scenario["steps"]
-     scenario_steps = ""
-     scenario_steps_array.each do |a|
-       scenario_steps << "#{a["keyword"]}#{a["name"]}\n"
-     end
-     params =  {
-       "title" => scenario_name,
-       "type_id" => 2,
-       "priority_id" => 5,
-       "estimate" => "3m",
-       "milestone" => "4.1",
-       "custom_steps" => scenario_steps,
-       "custom_expected" => "See steps for Validation"
-     }
+      scenario_name = scenario["name"]
+      scenario_steps_array = scenario["steps"]
+      scenario_steps = ""
+      scenario_steps_array.each do |a|
+        scenario_steps << "#{a["keyword"]}#{a["name"]}\n"
+      end
+      params =  {
+          "title" => scenario_name,
+          "type_id" => 2,
+          "priority_id" => 5,
+          "estimate" => "3m",
+          "milestone" => "4.1",
+          "custom_steps" => scenario_steps,
+          "custom_expected" => "See steps for Validation"
+      }
       test_case = client.send_post("add_case/#{section_id}", params)
-      cases_ids << test_case["id"]
+      $cases_ids << test_case["id"]
     end
-  
-  ##################################################################################################################
-  #   Create Test Run based on earlier added test cases
-  
-  
+
+    puts "Test Cases Added to TestRail"
+    ##################################################################################################################
+    #   Create Test Run based on earlier added test cases
+
+
     run_params =  {
-      "name" => "Automated: " + feature_name + " | " + Time.now.strftime("%d/%m/%Y %H:%M"),
-      "description" => feature_description,
-      "assign_to" => 1,
-      "include_all" => false,
-      "case_ids" => cases_ids
+        "name" => "Automated: " + feature_name + " | " + Time.now.strftime("%d/%m/%Y %H:%M"),
+        "description" => feature_description,
+        "assign_to" => 1,
+        "include_all" => false,
+        "case_ids" => $cases_ids,
+        "suite_id" => suite_id
     }
-  
-  
-  run = client.send_post("add_run/#{project_id}", run_params) 
-  run_id = run["id"]
-  
-  ##################################################################################################################
-  #   Parse test results
- 
-  case_result = "1" # set test case result
- 
- 
-  ##################################################################################################################
-  #   Push test results for test run to TestRail
-  
-  
-  cases_ids.each do |case_id|
-    result = { 
-      "case_id" => case_id, 
-      "status_id" => case_result, 
-      "comment" => "Test Comment" 
-    }
-      
-      #binding.pry
-    test_results << result
-  end  
-  
-  results_params =  { "results" => test_results }
-  
-  
-  client.send_post("add_results_for_cases/#{run_id}", results_params) 
-  
 
 
-    
-  
-   
- 
-  
+    run = client.send_post("add_run/#{project_id}", run_params)
+    run_id = run["id"]
+
+
+    puts "TestRun created"
+    ##################################################################################################################
+    #   Run test suite
+    puts "Running test suite"
+
+    puts "\nTest Run is commented out!!!\n"
+     #puts %x(cd ~/gitRepos/ExpediaBookingsUITests/Tablets_UI && bundle exec cucumber -p standard /Users/sbarylau/gitRepos/ExpediaBookingsUITests/Tablets_UI/features/in_development/test.feature)
+
+    puts "Test run finished"
+    ##################################################################################################################
+    #   Parse test results
+    puts "Parsing Test results"
+
+    xml_path = "/Users/sbarylau/gitRepos/ExpediaBookingsUITests/Tablets_UI/junit_format/TEST--Users-sbarylau-gitRepos-ExpediaBookingsUITests-Tablets_UI-features-in_development-test.xml"
+
+
+    doc = Nokogiri::XML( File.open(xml_path) )
+    fails = doc.xpath("//testsuite/testcase").map { |node| node.xpath("failure") }
+    parse_result = []
+    fails.each { |r| r.empty? ? parse_result << "5" : parse_result << "1" }
+
+
+    cases_with_results = (Hash[$cases_ids.zip(parse_result)]).to_a
+
+    puts "Test Results parsed"
+    ##################################################################################################################
+    #   Push test results for test run to TestRail
+    puts "Pushing Test Results"
+
+
+    cases_with_results.each do |case_result|
+      result = {
+          "case_id" => case_result[0],
+          "status_id" =>  case_result[1],
+          "comment" => "Test Comment"
+      }
+
+      test_results << result
+    end
+
+    results_params =  { "results" => test_results }
+
+
+    client.send_post("add_results_for_cases/#{run_id}", results_params)
+
+
+    ##################################################################################################################
+    puts " << SUCCESS: Test results pushed to TestRail!!! -;) >> "
+
+
+end # global features END
+
+
+
+
+
+
+
+
+
---
+++ legacy_init/init_1.rb
@@ -0,0 +1,120 @@
+require 'pry'
+require 'wally'
+require_relative 'parser.rb'
+require_relative 'testrail.rb'
+
+##################################################################################################################
+#   Define Variables
+
+client = TestRail::APIClient.new('https://expediabookings.testrail.com')
+client.user = 'sbarylau@expedia.com'
+client.password = 'Valerik26'
+
+project_id = "1"
+parent_id = "10" # Automated section in TestRail
+
+cases_ids =  [] # stores IDs of added test cases
+test_results = [] # stores test results after run
+##################################################################################################################
+# feature = { :name => $features.first[:gherkin]["name"], :description => $features.first[:gherkin]["description"],
+#   :tags => $features.first[:gherkin]["tags"], :scenarios =>  $features.first[:gherkin]["elements"] }
+
+  steps = ""
+  scenarios =  $features.first[:gherkin]["elements"]
+  feature_name = $features.first[:gherkin]["name"]
+  feature_description = $features.first[:gherkin]["description"]
+  feature_tags = $features.first[:gherkin]["tags"]
+  scenarios_array =  $features.first[:gherkin]["elements"]
+  first_scenario = $features.first[:gherkin]["elements"][0]
+  first_scenario_name = $features.first[:gherkin]["elements"][0]["name"]
+  first_scenario_tags = $features.first[:gherkin]["elements"][0]["tags"]
+  first_scenario_type = $features.first[:gherkin]["elements"][0]["type"]
+  first_scenario_steps_array = $features.first[:gherkin]["elements"][0]["steps"]
+  first_scenario_steps_array.each do |a|
+    steps << "#{a["keyword"]}#{a["name"]}\n"
+  end
+
+  section_params =  {
+    "name" => feature_name,
+    "description" => feature_description,
+    "parent_id" => parent_id
+  }
+  
+##################################################################################################################
+#   Create Section  
+  
+    post_section = client.send_post("add_section/#{project_id}", section_params) 
+    section_id =  post_section["id"]
+
+##################################################################################################################
+# Add Test Cases to earlier created section
+
+    scenarios_array.each do |scenario|
+     scenario_name = scenario["name"]
+     scenario_steps_array = scenario["steps"]
+     scenario_steps = ""
+     scenario_steps_array.each do |a|
+       scenario_steps << "#{a["keyword"]}#{a["name"]}\n"
+     end
+     params =  {
+       "title" => scenario_name,
+       "type_id" => 2,
+       "priority_id" => 5,
+       "estimate" => "3m",
+       "milestone" => "4.1",
+       "custom_steps" => scenario_steps,
+       "custom_expected" => "See steps for Validation"
+     }
+      test_case = client.send_post("add_case/#{section_id}", params)
+      cases_ids << test_case["id"]
+    end
+  
+  ##################################################################################################################
+  #   Create Test Run based on earlier added test cases
+  
+  
+    run_params =  {
+      "name" => "Automated: " + feature_name + " | " + Time.now.strftime("%d/%m/%Y %H:%M"),
+      "description" => feature_description,
+      "assign_to" => 1,
+      "include_all" => false,
+      "case_ids" => cases_ids
+    }
+  
+  
+  run = client.send_post("add_run/#{project_id}", run_params) 
+  run_id = run["id"]
+  
+  ##################################################################################################################
+  #   Parse test results
+ 
+  case_result = "1" # set test case result
+ 
+ 
+  ##################################################################################################################
+  #   Push test results for test run to TestRail
+  
+  
+  cases_ids.each do |case_id|
+    result = { 
+      "case_id" => case_id, 
+      "status_id" => case_result, 
+      "comment" => "Test Comment" 
+    }
+      
+      #binding.pry
+    test_results << result
+  end  
+  
+  results_params =  { "results" => test_results }
+  
+  
+  client.send_post("add_results_for_cases/#{run_id}", results_params) 
+  
+
+
+    
+  
+   
+ 
+  
