6403b18d44031a398fbf904f72619a4b8f0163f2
Siarhei Barylau <sbarylau@expedia.com>
2015-02-10 23:57:01 -0800

    Fixes broken vars in prse_test_results

--- lib/parse_test_results.rb
+++ lib/parse_test_results.rb
@@ -11,16 +11,16 @@ class ParseTestResults < RailRequest
 
 
   def find_junit_report_files
-    junit_folder_path = "#{$project_path}/Phones_UI/junit_format"
-    junit_folder_content = Dir.entries (junit_folder_path)
-    junit_folder_content.delete_if { |i| i == "." || i == ".."}
+    @junit_folder_path = "#{$project_path}/Phones_UI/junit_format"
+    @junit_folder_content = Dir.entries (@junit_folder_path)
+    @junit_folder_content.delete_if { |i| i == "." || i == ".."}
   end
 
 
   def scan_junit_files
     @parse_result = []
-    junit_folder_content.each do |file|
-      xml_path = junit_folder_path + "/" + file
+    @junit_folder_content.each do |file|
+      xml_path = @junit_folder_path + "/" + file
       doc = Nokogiri::XML( File.open(xml_path) )
       fails = doc.xpath("//testsuite/testcase").map { |node| node.xpath("failure") }
       fails.each { |r| r.empty? ? @parse_result << "1" : @parse_result << "5" }
